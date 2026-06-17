97a742e7c21642536920362bf88ba6aa18a60b0c
Vinicius Camargo <111100532+vinicamargors@users.noreply.github.com>
2026-06-17 20:23:31 -0300

    Change API URL to production environment

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "https://aip2fatec-production.up.railway.app/";
 
 async function apiGetCasos() {
   const res = await fetch(`${API_URL}/casos`);
